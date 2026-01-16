76730cee06b1657df3df776d9f2bda9a7e46f062
david-o-rosario1201 <finnnn023@gmail.com>
2026-01-16 16:47:57 -0400

    Agrega la anotación @AndroidEntryPoint y que extienda de AppCompatActivity()

--- app/src/main/java/edu/ucne/keepfocus/MainActivity.kt
+++ app/src/main/java/edu/ucne/keepfocus/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,9 +12,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.keepfocus.ui.theme.KeepFocusTheme
 
-class MainActivity : ComponentActivity() {
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
