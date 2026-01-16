a1cfa986021db02797d434dd1cdb73c9f7535497
david-o-rosario1201 <finnnn023@gmail.com>
2026-01-16 18:20:10 -0400

    Llama al archivo KeepFocusNavHost en el MainActivity

--- app/src/main/java/edu/ucne/keepfocus/MainActivity.kt
+++ app/src/main/java/edu/ucne/keepfocus/MainActivity.kt
@@ -1,18 +1,14 @@
 package edu.ucne.keepfocus
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.keepfocus.presentation.navigation.KeepFocusNavHost
 import edu.ucne.keepfocus.ui.theme.KeepFocusTheme
 
 @AndroidEntryPoint
@@ -22,29 +18,16 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContent {
             KeepFocusTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+               KeepFocusNavHost(navController = rememberNavController())
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     KeepFocusTheme {
-        Greeting("Android")
+       KeepFocusNavHost(navController = rememberNavController())
     }
 }
