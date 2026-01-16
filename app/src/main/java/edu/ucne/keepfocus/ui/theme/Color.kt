d56642639875dd4dcf03f9c3c2c22788e5b0a055
david-o-rosario1201 <finnnn023@gmail.com>
2026-01-16 16:47:23 -0400

    Agrega los colores que vas a usar en la app de KeepFocus

--- app/src/main/java/edu/ucne/keepfocus/ui/theme/Color.kt
+++ app/src/main/java/edu/ucne/keepfocus/ui/theme/Color.kt
@@ -9,3 +9,15 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+//Mis colores
+val FocusPrimary = Color(0xFF2CB9B0)
+val FocusSecondary = Color(0xFF1E88E5)
+val FocusSuccess = Color(0xFF43A047)
+val FocusAlert = Color(0xFFE53935)
+
+val FocusBackgroundLight = Color(0xFFF2F2F2)
+val FocusBackgroundDark = Color(0xFF121212)
+
+val FocusTextLight = Color(0xFF424242)
+val FocusTextDark = Color(0xFFE0E0E0)
--- app/src/main/java/edu/ucne/keepfocus/ui/theme/Theme.kt
+++ app/src/main/java/edu/ucne/keepfocus/ui/theme/Theme.kt
@@ -9,29 +9,39 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+    primary = FocusPrimary,
+    secondary = FocusSecondary,
+    tertiary = FocusSuccess,
+    background = FocusBackgroundLight,
+    surface = Color.White,
     onPrimary = Color.White,
     onSecondary = Color.White,
     onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    onBackground = FocusTextLight,
+    onSurface = FocusTextLight,
+    error = FocusAlert,
+    onError = Color.White
 )
+
+private val DarkColorScheme = darkColorScheme(
+    primary = FocusPrimary,
+    secondary = FocusSecondary,
+    tertiary = FocusSuccess,
+    background = FocusBackgroundDark,
+    surface = Color(0xFF1E1E1E),
+    onPrimary = Color.Black,
+    onSecondary = Color.White,
+    onTertiary = Color.Black,
+    onBackground = FocusTextDark,
+    onSurface = FocusTextDark,
+    error = FocusAlert,
+    onError = Color.Black
+)
+
 
 @Composable
 fun KeepFocusTheme(
