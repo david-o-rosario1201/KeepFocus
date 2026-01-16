7e49c5433a30342f7b506abeec92c86a17058707
david-o-rosario1201 <finnnn023@gmail.com>
2026-01-16 16:04:57 -0400

    Agrega nuevos plugins, actualiza la version de compileSdk = 36 y targetSdk = 36, agrega el room y agrega las siguientes dependencias: - navigation - room - hilt - splash api - icon api - Gson - DataStore - Biometric - AppCompat

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,21 +1,29 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
+    id("androidx.room")
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+    alias(libs.plugins.kotlinx.serialization)
 }
 
 android {
     namespace = "edu.ucne.keepfocus"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "edu.ucne.keepfocus"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    room {
+        schemaDirectory("$projectDir/schemas")
     }
 
     buildTypes {
@@ -56,4 +64,37 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //navigation
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.kotlinx.serialization.json)
+
+    //room
+    implementation("androidx.room:room-runtime:2.6.1")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1") // Asegúrate de utilizar 'room-compiler' aquí
+    implementation("androidx.room:room-ktx:2.6.1")
+
+    //hilt
+    implementation("com.google.dagger:hilt-android:2.51.1")
+    ksp("com.google.dagger:hilt-android-compiler:2.51.1")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+
+    //splash api
+    implementation("androidx.core:core-splashscreen:1.0.1")
+
+    //icon api
+    implementation("androidx.compose.material:material-icons-extended:1.7.5")
+
+    //Gson
+    implementation ("com.google.code.gson:gson:2.10.1")
+
+    //DataStore
+    implementation("androidx.datastore:datastore-preferences:1.1.7")
+
+    //Biometric
+    implementation("androidx.biometric:biometric:1.1.0")
+
+    //AppCompat
+    implementation("androidx.appcompat:appcompat:1.6.1")
 }
