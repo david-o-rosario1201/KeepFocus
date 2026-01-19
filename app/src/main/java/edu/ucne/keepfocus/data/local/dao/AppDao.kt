44988af6b3f1948bd41ab4a2b7cee0bf0d03e49d
david-o-rosario1201 <finnnn023@gmail.com>
2026-01-19 15:05:15 -0400

    Crea AppDao

---
+++ app/src/main/java/edu/ucne/keepfocus/data/local/dao/AppDao.kt
@@ -0,0 +1,27 @@
+package edu.ucne.keepfocus.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import edu.ucne.keepfocus.data.local.entities.AppEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface AppDao {
+    @Upsert
+    suspend fun upsertApp(app: AppEntity)
+
+    @Query("""
+        SELECT * FROM Apps
+        WHERE appId = :appId
+        LIMIT 1
+    """)
+    suspend fun getAppById(appId: Int): AppEntity?
+
+    @Delete
+    suspend fun deleteApp(app: AppEntity)
+
+    @Query("SELECT * FROM Apps")
+    suspend fun getApps(): Flow<List<AppEntity>>
+}
