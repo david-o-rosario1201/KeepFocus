8c8a2b680dfe2ce58128cff759cacf9c10743e49
david-o-rosario1201 <finnnn023@gmail.com>
2026-01-20 18:47:21 -0400

    Cambia el tipo de dato a FocusZoneWithApps

--- app/src/main/java/edu/ucne/keepfocus/domain/repositories/FocusZoneWithAppsRepository.kt
+++ app/src/main/java/edu/ucne/keepfocus/domain/repositories/FocusZoneWithAppsRepository.kt
@@ -1,9 +1,10 @@
 package edu.ucne.keepfocus.domain.repositories
 
 import edu.ucne.keepfocus.domain.models.FocusZone
+import edu.ucne.keepfocus.domain.models.FocusZoneWithApps
 import kotlinx.coroutines.flow.Flow
 
 interface FocusZoneWithAppsRepository {
-    suspend fun getFocusZoneWithApps(focusZoneId: Int): Flow<FocusZone>
-    suspend fun getFocusZoneWithApps(): Flow<List<FocusZone>>
+    suspend fun getFocusZoneWithApps(focusZoneId: Int): Flow<FocusZoneWithApps>?
+    suspend fun getFocusZoneWithApps(): Flow<List<FocusZoneWithApps>>
 }
