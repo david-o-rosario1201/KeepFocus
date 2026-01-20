029a25d441aaaebab64bbf6a31fd357ef4cb62ec
david-o-rosario1201 <finnnn023@gmail.com>
2026-01-20 18:48:12 -0400

    Crea un repository llamado FocusZoneWithAppsRepositoryImpl que utilice los métodos de FocusZoneWithAppsRepository

---
+++ app/src/main/java/edu/ucne/keepfocus/data/repositories/FocusZoneWithAppsRepositoryImpl.kt
@@ -0,0 +1,25 @@
+package edu.ucne.keepfocus.data.repositories
+
+import edu.ucne.keepfocus.data.local.dao.FocusZoneWithAppsDao
+import edu.ucne.keepfocus.data.mappers.toDomain
+import edu.ucne.keepfocus.domain.models.FocusZoneWithApps
+import edu.ucne.keepfocus.domain.repositories.FocusZoneWithAppsRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class FocusZoneWithAppsRepositoryImpl @Inject constructor(
+    private val focusZoneWithAppsDao: FocusZoneWithAppsDao
+): FocusZoneWithAppsRepository{
+    override suspend fun getFocusZoneWithApps(focusZoneId: Int): Flow<FocusZoneWithApps>? {
+        return focusZoneWithAppsDao.getFocusZoneWithApps(focusZoneId)
+            ?.map { focusZoneWithApp -> focusZoneWithApp.toDomain() }
+    }
+
+    override suspend fun getFocusZoneWithApps(): Flow<List<FocusZoneWithApps>> {
+        return focusZoneWithAppsDao.getFocusZoneWithApps()
+            .map { focusZoneWithApps ->
+                focusZoneWithApps.map { it.toDomain() }
+            }
+    }
+}
