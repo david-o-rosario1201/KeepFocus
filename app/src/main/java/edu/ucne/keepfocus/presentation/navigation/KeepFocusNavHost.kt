4475008d0535d9c255147e9e3243d321a806bb79
david-o-rosario1201 <finnnn023@gmail.com>
2026-01-21 13:39:47 -0400

    Elimina el navController como parámetro, crealo en el componente principal de navegación

--- app/src/main/java/edu/ucne/keepfocus/presentation/navigation/KeepFocusNavHost.kt
+++ app/src/main/java/edu/ucne/keepfocus/presentation/navigation/KeepFocusNavHost.kt
@@ -3,13 +3,15 @@ package edu.ucne.keepfocus.presentation.navigation
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
 
 @Composable
-fun KeepFocusNavHost(
-    navController: NavHostController
-){
+fun KeepFocusNavHost(){
+    val navController = rememberNavController()
     NavHost(
         navController = navController,
         startDestination = ""
-    ) { }
+    ) {
+
+    }
 }
