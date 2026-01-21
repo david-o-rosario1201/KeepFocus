0db1265dee4362b6aec2930eb10692c7ad0b6b0f
david-o-rosario1201 <finnnn023@gmail.com>
2026-01-21 12:34:36 -0400

    Crea un Modulo para el uso del sistema

---
+++ app/src/main/java/edu/ucne/keepfocus/di/SystemModule.kt
@@ -0,0 +1,16 @@
+package edu.ucne.keepfocus.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.keepfocus.data.system.AppUsageSystemImpl
+import edu.ucne.keepfocus.domain.system.AppUsageSystem
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class SystemModule {
+
+    @Binds
+    abstract fun bindsAppUsageSystem(impl: AppUsageSystemImpl): AppUsageSystem
+}
