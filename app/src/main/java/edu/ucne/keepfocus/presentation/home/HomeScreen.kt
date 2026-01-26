d796aac2385ee7552e7adabf59b5acc03fdf1f6c
david-o-rosario1201 <finnnn023@gmail.com>
2026-01-26 17:03:42 -0400

    Utiliza el componente TopAppBarComponent

--- app/src/main/java/edu/ucne/keepfocus/presentation/home/HomeScreen.kt
+++ app/src/main/java/edu/ucne/keepfocus/presentation/home/HomeScreen.kt
@@ -1,22 +1,12 @@
 package edu.ucne.keepfocus.presentation.home
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import edu.ucne.keepfocus.presentation.component.TopAppBarComponent
 
 @Composable
 fun HomeScreen(
@@ -25,41 +15,12 @@ fun HomeScreen(
     HomeBodyScreen()
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun HomeBodyScreen(
 
 ){
     Scaffold(
-        topBar = {
-            CenterAlignedTopAppBar(
-                title = {
-                    Column {
-                        Text(
-                            text = "!Hola, Juan Pérez",
-                            fontWeight = FontWeight.Bold,
-                            fontSize = 20.sp,
-                            color = MaterialTheme.colorScheme.onPrimary
-                        )
-                        Spacer(modifier = Modifier.height(10.dp))
-                        Text(
-                            text = "Controla tu tiempo en pantalla.",
-                            fontWeight = FontWeight.Normal,
-                            fontSize = 15.sp,
-                            color = MaterialTheme.colorScheme.onPrimary.copy(alpha = 0.9f)
-                        )
-                    }
-                },
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primary,
-                    titleContentColor = MaterialTheme.colorScheme.onPrimary
-                ),
-                modifier = Modifier.shadow(
-                    elevation = 8.dp,
-                    shape = RoundedCornerShape(bottomStart = 30.dp, bottomEnd = 30.dp)
-                )
-            )
-        }
+        topBar = { TopAppBarComponent() }
     ){ innerPadding ->
         Column(
             modifier = Modifier.padding(innerPadding)
