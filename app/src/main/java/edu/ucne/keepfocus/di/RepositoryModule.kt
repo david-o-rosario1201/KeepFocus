c01a3860968ff14e0b057666dc4f9287cf721c82
david-o-rosario1201 <finnnn023@gmail.com>
2026-01-27 16:16:24 -0400

    Elimina el repositorio de FocusZoneWithAppsRepository

--- app/src/main/java/edu/ucne/keepfocus/di/RepositoryModule.kt
+++ app/src/main/java/edu/ucne/keepfocus/di/RepositoryModule.kt
@@ -7,11 +7,9 @@ import dagger.hilt.components.SingletonComponent
 import edu.ucne.keepfocus.data.repositories.AppRepositoryImpl
 import edu.ucne.keepfocus.data.repositories.DetalleFocusZoneAppRepositoryImpl
 import edu.ucne.keepfocus.data.repositories.FocusZoneRepositoryImpl
-import edu.ucne.keepfocus.data.repositories.FocusZoneWithAppsRepositoryImpl
 import edu.ucne.keepfocus.domain.repositories.AppRepository
 import edu.ucne.keepfocus.domain.repositories.DetalleFocusZoneAppRepository
 import edu.ucne.keepfocus.domain.repositories.FocusZoneRepository
-import edu.ucne.keepfocus.domain.repositories.FocusZoneWithAppsRepository
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -25,7 +23,4 @@ abstract class RepositoryModule {
 
     @Binds
     abstract fun bindsDetalleFocusZoneAppRepository(impl: DetalleFocusZoneAppRepositoryImpl): DetalleFocusZoneAppRepository
-
-    @Binds
-    abstract fun bindsFocusZoneWithAppsRepository(impl: FocusZoneWithAppsRepositoryImpl): FocusZoneWithAppsRepository
 }
