4951b1aa430a484402db371726807c05b23ef81a
david-o-rosario1201 <finnnn023@gmail.com>
2026-01-27 16:14:34 -0400

    Agrega la lógica para obtener los focus con las apps en los archivos de FocusZone

--- app/src/main/java/edu/ucne/keepfocus/data/local/dao/FocusZoneDao.kt
+++ app/src/main/java/edu/ucne/keepfocus/data/local/dao/FocusZoneDao.kt
@@ -3,8 +3,10 @@ package edu.ucne.keepfocus.data.local.dao
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Query
+import androidx.room.Transaction
 import androidx.room.Upsert
 import edu.ucne.keepfocus.data.local.entities.FocusZoneEntity
+import edu.ucne.keepfocus.data.local.entities.FocusZoneWithAppsEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -17,11 +19,22 @@ interface FocusZoneDao {
         WHERE focusZoneId = :focusZoneId
         LIMIT 1
     """)
-    fun getFocusZoneById(focusZoneId: Int): Flow<FocusZoneEntity>?
+    fun observeFocusZoneById(focusZoneId: Int): Flow<FocusZoneEntity?>
 
     @Delete
     suspend fun deleteFocusZone(focusZone: FocusZoneEntity)
 
     @Query("SELECT * FROM FocusZones")
-    fun getFocusZones(): Flow<List<FocusZoneEntity>>
+    fun observeFocusZones(): Flow<List<FocusZoneEntity>>
+
+    @Transaction
+    @Query("""
+        SELECT * FROM FocusZones
+        WHERE focusZoneId = :focusZoneId
+    """)
+    fun observeFocusZoneWithApps(focusZoneId: Int): Flow<FocusZoneWithAppsEntity?>
+
+    @Transaction
+    @Query("SELECT * FROM FocusZones")
+    fun observeFocusZonesWithApps(): Flow<List<FocusZoneWithAppsEntity>>
 }
--- app/src/main/java/edu/ucne/keepfocus/domain/repositories/FocusZoneRepository.kt
+++ app/src/main/java/edu/ucne/keepfocus/domain/repositories/FocusZoneRepository.kt
@@ -1,11 +1,14 @@
 package edu.ucne.keepfocus.domain.repositories
 
 import edu.ucne.keepfocus.domain.models.FocusZone
+import edu.ucne.keepfocus.domain.models.FocusZoneWithApps
 import kotlinx.coroutines.flow.Flow
 
 interface FocusZoneRepository {
     suspend fun upsertFocusZone(focusZone: FocusZone)
-    fun getFocusZoneById(focusZoneId: Int): Flow<FocusZone>?
+    fun observeFocusZoneById(focusZoneId: Int): Flow<FocusZone?>
     suspend fun deleteFocusZone(focusZone: FocusZone)
-    fun getFocusZones(): Flow<List<FocusZone>>
+    fun observeFocusZones(): Flow<List<FocusZone>>
+    fun observeFocusZoneWithApps(focusZoneId: Int): Flow<FocusZoneWithApps?>
+    fun observeFocusZonesWithApps(): Flow<List<FocusZoneWithApps>>
 }
