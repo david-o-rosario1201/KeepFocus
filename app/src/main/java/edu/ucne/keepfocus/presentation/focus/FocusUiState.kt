561f7f3c9931f4b65f9f12b2bacd901f3b4368f3
david-o-rosario1201 <finnnn023@gmail.com>
2026-02-17 17:19:58 -0400

    Agrega las variables nombre, icono y tiempoLimite

--- app/src/main/java/edu/ucne/keepfocus/presentation/focus/FocusUiState.kt
+++ app/src/main/java/edu/ucne/keepfocus/presentation/focus/FocusUiState.kt
@@ -1,6 +1,9 @@
 package edu.ucne.keepfocus.presentation.focus
 
 data class FocusUiState(
+    val nombre: String = "",
+    val icono: String = "",
+    val tiempoLimite: Long = 0,
     val installedApps: List<AppUi> = emptyList(),
     val showAppPickerModal: Boolean = false,
     val selectedApps: List<AppUi> = emptyList()
