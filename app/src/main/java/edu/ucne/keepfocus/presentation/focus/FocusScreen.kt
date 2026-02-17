48edd5484ca766e897ff1678f9082ce2e9c7c324
david-o-rosario1201 <finnnn023@gmail.com>
2026-02-17 15:14:23 -0400

    Agrega FocusViewModel como parámetro, crea una variable llamada uiState, pasa uiState y onEvent a la función FocusBodyScreen, crea una variable llamada apps para almacenar la lista de aplicaciones instaladas, agrega el evento para abrir el modal, crea los componentes EmptyAppsList, SelectedAppsList, AppModalPicker, OptionItem, SmallRadioButton

--- app/src/main/java/edu/ucne/keepfocus/presentation/focus/FocusScreen.kt
+++ app/src/main/java/edu/ucne/keepfocus/presentation/focus/FocusScreen.kt
@@ -1,6 +1,13 @@
+@file:OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class)
+
 package edu.ucne.keepfocus.presentation.focus
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,14 +18,22 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.QuestionMark
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
@@ -31,6 +46,7 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -42,23 +58,36 @@ import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.hilt.navigation.compose.hiltViewModel
+import edu.ucne.keepfocus.R
 import edu.ucne.keepfocus.presentation.component.TopAppBarComponent
 
 @Composable
 fun FocusScreen(
-
+    viewModel: FocusViewModel = hiltViewModel()
 ){
-    FocusBodyScreen()
+    val uiState by viewModel.uiState.collectAsState()
+    FocusBodyScreen(
+        uiState = uiState,
+        onEvent = viewModel::onEvent
+    )
 }
 
 @Composable
 private fun FocusBodyScreen(
-
+    uiState: FocusUiState,
+    onEvent: (FocusUiEvent) -> Unit
 ){
+    val apps = uiState.installedApps
+
     Scaffold(
         topBar = { TopAppBarComponent(title = "Registrar un Nuevo Focus", subtitle = "") }
     ){ innerPadding ->
@@ -77,7 +106,7 @@ private fun FocusBodyScreen(
             )
             Spacer(modifier = Modifier.height(25.dp))
             Button(
-                onClick = {},
+                onClick = { onEvent(FocusUiEvent.OnOpenModal) },
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(54.dp)
@@ -93,6 +122,116 @@ private fun FocusBodyScreen(
                     fontWeight = FontWeight.Bold,
                     fontSize = 18.sp
                 )
+            }
+
+            if(uiState.showAppPickerModal){
+                AppModalPicker(
+                    apps = apps,
+                    onEvent = onEvent,
+                    onDismiss = { onEvent(FocusUiEvent.OnCloseModal) }
+                )
+            }
+
+            if(uiState.isEmpty){
+                EmptyAppsList()
+            } else{
+                SelectedAppsList(
+                    apps = uiState.selectedApps,
+                    onEvent = onEvent
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun EmptyAppsList(){
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(50.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.cajavacia),
+            contentDescription = "",
+            modifier = Modifier.size(100.dp)
+        )
+        Text(
+            text = "Aún no has seleccinado aplicaciones para este focus.",
+            fontWeight = FontWeight.Normal,
+            fontSize = 14.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.width(300.dp)
+        )
+    }
+}
+
+@Composable
+private fun SelectedAppsList(
+    apps: List<AppUi>,
+    onEvent: (FocusUiEvent) -> Unit
+){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = "Aplicaciones Seleccionadas:",
+            fontWeight = FontWeight.Bold,
+            fontSize = 15.sp,
+            color = MaterialTheme.colorScheme.onSurface,
+            modifier = Modifier.padding(top = 10.dp),
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 10.dp, bottom = 20.dp, start = 20.dp, end = 20.dp)
+                .border(
+                    width = 2.dp,
+                    color = MaterialTheme.colorScheme.primary,
+                    shape = RoundedCornerShape(10.dp)
+                )
+                .padding(20.dp)
+        ){
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ){
+                items(
+                    items = apps,
+                    key = { it.packageName }
+                ){ app ->
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically
+                    ){
+                        app.icon?.let {
+                            Image(
+                                bitmap = it,
+                                contentDescription = app.name,
+                                modifier = Modifier.size(45.dp)
+                            )
+                        }
+                        Spacer(modifier = Modifier.width(5.dp))
+                        Text(
+                            text = app.name,
+                            fontWeight = FontWeight.Normal,
+                            fontSize = 15.sp,
+                            color = MaterialTheme.colorScheme.onSurface,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis,
+                            modifier = Modifier.weight(1f)
+                        )
+                        IconButton(
+                            onClick = { onEvent(FocusUiEvent.OnDeleteSelectedApp(app.packageName))}
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Delete,
+                                contentDescription = "Eliminar app de la lista"
+                            )
+                        }
+                    }
+                }
             }
         }
     }
@@ -212,5 +351,141 @@ private fun AssistedDropDown(
         }
 
         HelpButton(onButtonClick = onButtonClick)
+    }
+}
+
+@Composable
+private fun AppModalPicker(
+    apps: List<AppUi>,
+    onEvent: (FocusUiEvent) -> Unit,
+    onDismiss: () -> Unit
+){
+    Dialog(onDismissRequest = onDismiss) {
+        Card(
+            shape = RoundedCornerShape(16.dp),
+            colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface)
+        ){
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Text(
+                    text = "Aplicaciones",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+                Text(
+                    text = "Selecciona las aplicaciones que deseas agregar a la lista.",
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 18.sp,
+                    color = MaterialTheme.colorScheme.onSurface,
+                    textAlign = TextAlign.Center
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                LazyVerticalGrid(
+                    columns = GridCells.Adaptive(80.dp),
+                    modifier = Modifier.height(300.dp),
+                    verticalArrangement = Arrangement.spacedBy(12.dp)
+                ){
+                    items(apps){ app ->
+                        OptionItem(
+                            app = app,
+                            selected = app.isSelected,
+                            onSelected = { onEvent(FocusUiEvent.OnSelectedApp(app.packageName)) }
+                        )
+                    }
+                }
+                Spacer(modifier = Modifier.height(20.dp))
+                Button(
+                    onClick = { onEvent(FocusUiEvent.OnAddSelectedApps(
+                        apps.filter { it.isSelected }
+                    )) },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(54.dp)
+                        .padding(horizontal = 10.dp, vertical = 3.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = MaterialTheme.colorScheme.secondary,
+                        contentColor = MaterialTheme.colorScheme.onSecondary
+                    ),
+                    shape = RoundedCornerShape(12.dp)
+                ) {
+                    Text(
+                        text = "Continuar",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun OptionItem(
+    app: AppUi,
+    selected: Boolean,
+    onSelected: () -> Unit
+){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onSelected() }
+    ){
+        Text(
+            text = app.name,
+            fontWeight = if(selected) FontWeight.Bold else FontWeight.Normal,
+            fontSize = 12.sp,
+            color =  MaterialTheme.colorScheme.onSurface,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+        app.icon?.let {
+            Image(
+                bitmap = it,
+                contentDescription = app.name,
+                modifier = Modifier.size(48.dp)
+            )
+        }
+        Spacer(modifier = Modifier.height(5.dp))
+        SmallRadioButton(
+            selected = selected,
+            onButtonClick = onSelected
+        )
+    }
+}
+
+@Composable
+private fun SmallRadioButton(
+    selected: Boolean,
+    onButtonClick: () -> Unit
+){
+    Box(
+        modifier = Modifier
+            .size(18.dp)
+            .clip(CircleShape)
+            .border(
+                width = 2.dp,
+                color = MaterialTheme.colorScheme.primary,
+                shape = CircleShape
+            )
+            .clickable { onButtonClick() },
+        contentAlignment = Alignment.Center
+    ){
+        if(selected){
+            Box(
+                modifier = Modifier
+                    .size(10.dp)
+                    .clip(CircleShape)
+                    .background(MaterialTheme.colorScheme.primary)
+            )
+        }
     }
 }
