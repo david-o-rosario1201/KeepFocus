0af50530147e2041183f6d29bb51b21f36141e5c
david-o-rosario1201 <finnnn023@gmail.com>
2026-02-20 18:29:17 -0400

    Elimina la data class OnAddSelectedApps, ya no se va a utilizar

--- app/src/main/java/edu/ucne/keepfocus/presentation/focus/FocusUiEvent.kt
+++ app/src/main/java/edu/ucne/keepfocus/presentation/focus/FocusUiEvent.kt
@@ -5,7 +5,6 @@ sealed interface FocusUiEvent {
     data object Save: FocusUiEvent
     data class OnShowOverlay(val overlay: FocusOverlay): FocusUiEvent
     data class OnSelectedApp(val packageName: String): FocusUiEvent
-    data class OnAddSelectedApps(val apps: List<AppUi>): FocusUiEvent
     data class OnDeleteSelectedApp(val packageName: String): FocusUiEvent
     data class OnNombreChange(val nombre: String): FocusUiEvent
     data class OnTiempoLimiteChange(val tiempoLimite: Long): FocusUiEvent
