9033bf15d0d37f40a1f4c6b22f057498c39a30e2
david-o-rosario1201 <finnnn023@gmail.com>
2026-02-20 18:16:15 -0400

    Agrega el data object llamado Save

--- app/src/main/java/edu/ucne/keepfocus/presentation/focus/FocusUiEvent.kt
+++ app/src/main/java/edu/ucne/keepfocus/presentation/focus/FocusUiEvent.kt
@@ -2,6 +2,7 @@ package edu.ucne.keepfocus.presentation.focus
 
 sealed interface FocusUiEvent {
     data object OnDismissOverlay: FocusUiEvent
+    data object Save: FocusUiEvent
     data class OnShowOverlay(val overlay: FocusOverlay): FocusUiEvent
     data class OnSelectedApp(val packageName: String): FocusUiEvent
     data class OnAddSelectedApps(val apps: List<AppUi>): FocusUiEvent
