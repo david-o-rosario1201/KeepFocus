748d7ff11f7f006ddc5d63bef661c02563e71752
david-o-rosario1201 <finnnn023@gmail.com>
2026-03-02 21:09:35 -0400

    Agrega un effect AllowNavigation

--- app/src/main/java/edu/ucne/keepfocus/presentation/focus/FocusUiEffect.kt
+++ app/src/main/java/edu/ucne/keepfocus/presentation/focus/FocusUiEffect.kt
@@ -1,6 +1,7 @@
 package edu.ucne.keepfocus.presentation.focus
 
 sealed interface FocusUiEffect {
+    data object AllowNavigation: FocusUiEffect
     data class NavigateBackWithMessage(val message: String): FocusUiEffect
     data class ShowSnackbar(val message: String): FocusUiEffect
 }
