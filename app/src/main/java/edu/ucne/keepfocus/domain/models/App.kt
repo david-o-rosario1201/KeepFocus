a1eae3289ad342a0e23df8685eeeef7766bef520
david-o-rosario1201 <finnnn023@gmail.com>
2026-03-02 22:52:47 -0400

    Elimina la variable icon

--- app/src/main/java/edu/ucne/keepfocus/domain/models/App.kt
+++ app/src/main/java/edu/ucne/keepfocus/domain/models/App.kt
@@ -5,7 +5,6 @@ import android.graphics.drawable.Drawable
 data class App(
     val packageName: String,
     val name: String,
-    val icon: Drawable?,
     val isBlocked: Boolean,
     val dailyLimitMinutes: Int
 )
