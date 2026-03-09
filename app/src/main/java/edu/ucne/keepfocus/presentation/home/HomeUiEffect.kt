f38bde199b1be87b975da2fa70d3e3261083baa7
David Otoniel Rosario Serrano <finnnn023@gmail.com>
2026-03-09 12:02:24 -0400

    Crea una interface llamada HomeUiEffect

---
+++ app/src/main/java/edu/ucne/keepfocus/presentation/home/HomeUiEffect.kt
@@ -0,0 +1,6 @@
+package edu.ucne.keepfocus.presentation.home
+
+sealed interface HomeUiEffect {
+    data class ShowSnackbar(val message: String): HomeUiEffect
+    data class NavigateToEdit(val focusId: Int): HomeUiEffect
+}
