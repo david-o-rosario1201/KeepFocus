ca23fc5726b00959e72b1a831013fcc6e37f7101
David Otoniel Rosario Serrano <finnnn023@gmail.com>
2026-03-09 20:56:49 -0400

    Crea una data class ToggleAppSelection qeu sirve para seleccionr o deseleccionar una app, OnDeleteFocus para  eliminar un focus y OnSelectedFocus para guardar el focus seleccionado

--- app/src/main/java/edu/ucne/keepfocus/presentation/focus/FocusUiEvent.kt
+++ app/src/main/java/edu/ucne/keepfocus/presentation/focus/FocusUiEvent.kt
@@ -6,9 +6,10 @@ sealed interface FocusUiEvent {
     data object OnNavigationAttempt: FocusUiEvent
     data object OnConfirmExit: FocusUiEvent
     data class OnShowOverlay(val overlay: FocusOverlay): FocusUiEvent
-    data class OnSelectedApp(val packageName: String): FocusUiEvent
-    data class OnDeleteSelectedApp(val packageName: String): FocusUiEvent
+    data class ToggleAppSelection(val packageName: String): FocusUiEvent
     data class OnNombreChange(val nombre: String): FocusUiEvent
     data class OnTiempoLimiteChange(val tiempoLimite: Long): FocusUiEvent
     data class OnIconoChange(val icono: Int): FocusUiEvent
+    data class OnDeleteFocus(val focusId: Int): FocusUiEvent
+    data class OnSelectedFocus(val focusId: Int): FocusUiEvent
 }
