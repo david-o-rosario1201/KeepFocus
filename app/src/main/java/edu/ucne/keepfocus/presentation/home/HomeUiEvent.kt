0ab746004f1333596620d6ca33ceb68c442f6329
David Otoniel Rosario Serrano <finnnn023@gmail.com>
2026-04-01 17:22:03 -0400

    Crea la data class OnViewFocusDetails

--- app/src/main/java/edu/ucne/keepfocus/presentation/home/HomeUiEvent.kt
+++ app/src/main/java/edu/ucne/keepfocus/presentation/home/HomeUiEvent.kt
@@ -3,4 +3,5 @@ package edu.ucne.keepfocus.presentation.home
 sealed interface HomeUiEvent {
     data class OnDeleteFocus(val focusId: Int): HomeUiEvent
     data class OnEditFocus(val focusId: Int): HomeUiEvent
+    data class OnViewFocusDetails(val focusId: Int): HomeUiEvent
 }
