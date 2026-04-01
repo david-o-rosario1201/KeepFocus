16529969d78940f439fe5dd812a76ce33fc0cdcd
David Otoniel Rosario Serrano <finnnn023@gmail.com>
2026-04-01 17:21:41 -0400

    Crea la data class NavigateToDetails

--- app/src/main/java/edu/ucne/keepfocus/presentation/home/HomeUiEffect.kt
+++ app/src/main/java/edu/ucne/keepfocus/presentation/home/HomeUiEffect.kt
@@ -3,4 +3,5 @@ package edu.ucne.keepfocus.presentation.home
 sealed interface HomeUiEffect {
     data class ShowSnackbar(val message: String): HomeUiEffect
     data class NavigateToEdit(val focusId: Int): HomeUiEffect
+    data class NavigateToDetails(val focusId: Int): HomeUiEffect
 }
